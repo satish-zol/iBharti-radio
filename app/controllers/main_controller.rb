a1de0b27e713d8bd9db7b1c3180b0b8cf8ec004b
satish-zol <zsatish201@gmail.com>
2012-12-06 13:56:04 +0530

    song sort by id

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
   
   respond_to :json, :xml
   def index
-  	@songs = Song.order 'name'
+  	@songs = Song.order 'id'
   	#respond_with(@songs)
   	# respond_to do |format|
    #    format.html # index.html.erb
