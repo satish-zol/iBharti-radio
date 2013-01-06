0b24adb4f799f331c0d2a2ba697f1ab24ddaab98
satish <satish@satish-virtual-machine.(none)>
2013-01-06 19:27:44 +0530

    add confirmed_at to seed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ IbhartiRadio::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.action_mailer.default_url_options = { :host => 'http://ibharati-radio.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'ibharati-radio.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
