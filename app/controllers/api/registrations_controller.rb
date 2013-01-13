60ac36ca205a64c1b9fd4237ae56f7d31b1267fd
satish-zol <zsatish201@gmail.com>
2013-01-13 14:39:19 +0530

    change the ip for streaming

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -1,50 +1,52 @@
 class Api::RegistrationsController < Api::BaseController
+  before_filter :authenticate_user!, :only => :token
   
-  # respond_to :json
-  # def new
+  respond_to :json
+  def new
  
-  #   user = User.new(params[:user])
-  #   if user.save
-  #     render :json=> {:response_api => [user.as_json(:auth_token=>user.authentication_token, :email=>user.email), :status=>201]} 
-  #     return
-  #   else
-  #     warden.custom_failure!
-  #     render :json=> {:response_api => [user.errors, :status=>422]}
-  #   end
-  # end
+    user = User.new(params[:user])
+    if user.save
+      #debugger
+      render :json=> {:response_api => [:auth_token=>user.authentication_token, :email=>user.email, :status=>201]} 
+      return
+    else
+      warden.custom_failure!
+      render :json=> {:response_api => [user.errors, :status=>422]}
+    end
+  end
 
 
   # def new
   #   super
   # end
 
-  def new
-    resource = User.new(params[:resource])
-    if resource.save
-      flash[:notice] = "You have signed up successfully. A confirmation was sent to your e-mail."
-      respond_to do |format|
-        format.json {
-          if !params[:api_key].blank? and params[:api_key] == API_KEY
-            build_resource
-            if resource.save
-              sign_in(resource)
-              resource.reset_authentication_token!
-              render :template => '/devise/registrations/signed_up' #rabl template with authentication token
-            else
-              render :template => '/devise/registrations/new' #rabl template with errors
-            end
-          else
-            render :json => {'errors'=>{'api_key' => 'Invalid'}}.to_json, :status => 401
-          end
-        }
-        format.any{super}
-      end
-    else
-      render :action => :new
-    end
-  end
+  # def new
+  #   resource = User.new(params[:resource])
+  #   if resource.save
+  #     flash[:notice] = "You have signed up successfully. A confirmation was sent to your e-mail."
+  #     respond_to do |format|
+  #       format.json {
+  #         if !params[:api_key].blank? and params[:api_key] == API_KEY
+  #           build_resource
+  #           if resource.save
+  #             sign_in(resource)
+  #             resource.reset_authentication_token!
+  #             render :template => '/devise/registrations/signed_up' #rabl template with authentication token
+  #           else
+  #             render :template => '/devise/registrations/new' #rabl template with errors
+  #           end
+  #         else
+  #           render :json => {'errors'=>{'api_key' => 'Invalid'}}.to_json, :status => 401
+  #         end
+  #       }
+  #       format.any{super}
+  #     end
+  #   else
+  #     render :action => :new
+  #   end
+  # end
 
-  def update
-    super
-  end
+  # def update
+  #   super
+  # end
 end
