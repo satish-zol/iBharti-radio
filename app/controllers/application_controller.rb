7a8b3b6054756e8fb5a8efb78f24d935fbff716d
kulavi <avi@panhealth.com>
2013-11-09 14:44:30 +0530

    remove ruby shout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
-require "shout"
-require 'nokogiri'
-require 'open-uri'
+#require "shout"
+#require 'nokogiri'
+#require 'open-uri'
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
@@ -25,10 +25,10 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, :alert => exception.message
   end
 
-  def get_mountpoints(connection)
-    doc = get_icecast_status(connection)
-    doc.css('.streamheader h3').children.empty? ? false : doc.css('.streamheader h3').children.map{|c| c.text}
-  end
+  #def get_mountpoints(connection)
+   # doc = get_icecast_status(connection)
+    #doc.css('.streamheader h3').children.empty? ? false : doc.css('.streamheader h3').children.map{|c| c.text}
+  #end
 
 
   private 
@@ -43,18 +43,18 @@ class ApplicationController < ActionController::Base
     return @categories, @languages, @tags, @cities, @colleges
   end
 
-  def mount_point_for_clg
-    #debugger  
-    @s = Shout.new
-    @s.charset = "UTF-8"
-    @s.port = 8022
-    @s.host = "localhost"
-    @s.user = "source"
-    @s.pass = "bpsi@123"
-    @s.format = Shout::MP3
-    #@s.description ='song classic'
-    @s.mount = "#{@clg_name.mount_point}.m3u"
-    @s.connect unless get_mountpoints(@s.mount).present?
-  end 
+  # def mount_point_for_clg
+  #   #debugger  
+  #   @s = Shout.new
+  #   @s.charset = "UTF-8"
+  #   @s.port = 8022
+  #   @s.host = "localhost"
+  #   @s.user = "source"
+  #   @s.pass = "bpsi@123"
+  #   @s.format = Shout::MP3
+  #   #@s.description ='song classic'
+  #   @s.mount = "#{@clg_name.mount_point}.m3u"
+  #   @s.connect unless get_mountpoints(@s.mount).present?
+  # end 
 
 end
