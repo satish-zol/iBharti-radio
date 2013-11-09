4dc0d9ba3e5ebe11bbb94461cf5ece4df391020b
kulavi <avi@panhealth.com>
2013-11-09 15:42:45 +0530

    commit uncomment token_authenticable

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable#, :token_authenticatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :token_authenticatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :role_ids, :authentication_token, 
                   :confirmed_at
   # attr_accessible :title, :body
-	#before_save :ensure_authentication_token
+	before_save :ensure_authentication_token
 end
